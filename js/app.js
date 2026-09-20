73ebef79bdcd37dfa7b3d7fe2f0ba2bce18dfd6e
Nelsonwilliam131 <NelsonWilliamxx1@gmail.com>
2026-09-20 18:12:34 +0100

    Update print statement from 'Hello' to 'Goodbye'

--- js/app.js
+++ js/app.js
@@ -161,7 +161,9 @@ const CURATED = [
    {content: " At this stage of your life,  focus on building you capability", author: "GehGeh (Nigerian influencer"},
    {content: "Stop playing with your time, make good use of your time well", author: "GehGeh (Nigerian influencer"},
    {content: "How you see me nor really concern me, na how I see myself na important for life", author: "GehGeh(Nigerian influncer"),
-    {content: "How long you live nor really matter, now how well you take leave am na matter", author: "GehGeh(Nigerian influncer)"),
+   {content: "How long you live nor really matter, now how well you take leave am na matter", author: "GehGeh(Nigerian influncer)"),
+    //Game characters
+    "The right man in the wrong place can make all the difference in the world", author "G-man(Half-life 2)",
     
       
 ];
